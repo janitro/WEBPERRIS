f077ba3d2a298ce50bb5125b8bed0cfe9516b528
Fabian Plaza <fabianplaza@gmail.com>
2018-12-03 21:22:45 -0300

    Template static  work

--- misitio/settings.py
+++ misitio/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -147,12 +149,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'static/',
-]
-STATIC_ROOT = "static/"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     'static/',
+# ]
+STATIC_ROOT = 'static'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ('static/',)
 
 
 
